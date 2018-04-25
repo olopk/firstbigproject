54eca57878263f91c926f32f1a910cc3057d6727
olopk <olekwojas@gmail.com>
2018-04-25 15:13:36 +0200

    plus

--- app.js
+++ app.js
@@ -5,7 +5,17 @@ var budgetController = (function() {
 
 // UI CONTROLLER
 var UIController = (function(){
-    // some code
+    
+    return {
+        getInput: function(){
+            
+            return {
+                type: document.querySelector('.add__type').value, // will be either inc or exp
+                description: document.querySelector('.add__description').value,
+                value: document.querySelector('.add__value').value
+            };
+        }
+    };
     
 })();
 
@@ -14,11 +24,14 @@ var controller = (function(budgetCTRL, uiCTRL){
     var ctrlAddItem = function() {
         console.log('Enter was pressed');
         // 1. Get the field input data
+        var input = uiCTRL.getInput();
+        console.log(input);
         // 2. Add the item to the budget controller
         // 3. Add the item to te UI
         // 4. Calculate the budget
         // 5. Display the budget
     }
+   
     document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
     
     document.addEventListener('keypress', function(event){
